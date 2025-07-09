cd9dfdf2b491dac083033d3e323866a903ba665d
i-akguel03 <i.akguel@outlook.com>
2025-07-09 19:08:54 +0200

    Initial commit: Spring Boot Grundstruktur mit MongoDB Setup

---
+++ src/test/java/com/erp/backend/ErpSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
